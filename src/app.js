d99767e681c7de902675d706fb97cee078480bfc
Nicolas Daudin <nicolas.daudin@gmail.com>
2019-05-13 18:19:01 +0200

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import moment from 'moment';
 moment.locale('fr');
 
 const store = configureStore();
+console.log('testing sourcemaps');
 const jsx = (
   <Provider store={store}>
     <AppRouter />
